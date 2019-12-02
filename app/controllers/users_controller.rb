26fa2831aed14d18f7c563b697e8f1d2081e97b8
Anastasia Pankina <siapankina@gmail.com>
2019-12-02 14:23:05 +0000

    fixed some bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,19 @@
 class UsersController < ApplicationController
-    def new
-    end
-  
-    def create
-      @user = User.create(user_params)
-      return redirect_to controller: "users", action: "new" unless @user.save
-      session[:user_id] = @user.id
-      redirect_to controller: "welcome", action: "home"
-    end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:name, :password, :password_confirmation)
-    end
-  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    # return redirect_to controller: "users", action: "new" unless @user.save
+    # session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :bio, :age)
+  end
 end
