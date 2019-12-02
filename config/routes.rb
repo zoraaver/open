84b206d3d8f47c9212edadb1cf4fc13c5b52fd91
Anastasia Pankina <siapankina@gmail.com>
2019-12-02 13:53:49 +0000

    added login and create page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   resources :comments
   resources :friendships
   resources :users
+  resources :sessions
+
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  post "/logout" => "sessions#destroy"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
