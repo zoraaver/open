671ff8587116bb8dcf3109600d80e0356d2a3b1a
github username <github email address>
2019-12-02 14:31:29 +0000

    added user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  before_action :set_user
+
     def new
     end
   
@@ -8,11 +11,19 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to controller: "welcome", action: "home"
     end
+
+    def show
+
+    end
   
     private
   
     def user_params
       params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
   
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :post
+
+    def date
+        created_at.strftime("%A %d %B %H:%M")
+    end
+
 end
