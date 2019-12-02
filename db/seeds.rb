8e9dda08546eb4e172c89ee64c6b9cfec894c505
github username <github email address>
2019-12-02 12:34:11 +0000

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+i = 0
+
 10.times do
-    User.create(name: )
+    u = User.create(name: Faker::Name.unique.name, email: Faker::Internet.unique.email, password_digest: "password#{i}", age: rand(13..50), bio: Faker::Quote.famous_last_words)
+
+    3.times do 
+        u.posts.build(content: Faker::Quote.yoda, likes: 0).save
+    end
+
+    i += 1
+end
+
+i = 0
+users = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10]
+friends = [2,3,9,8,9,7,1,6,2,10,8,1,6,1,7,9,4,5,1,2]
+
+20.times do
+    Friendship.create(user_id: users[i], friend_id: friends[i], status: "accepted")
+    i += 1
+end
+
+100.times do
+    Comment.create(user: User.all.sample, post: Post.all.sample, content: Faker::Movie.quote)
 end
