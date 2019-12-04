6ec090089abf4611e5af8ec8bbc901bf164c2707
github username <github email address>
2019-12-04 15:07:01 +0000

    added mutual friends

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
   get "users/:id/friends" => "users#friend_page", as: :user_friends
+  get "users/:id/mutual-friends" => "users#mutual", as: :mutual_friends
   get "users/:id/friend_requests" => "users#friend_requests", as: :friend_requests
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
